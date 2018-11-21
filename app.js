27ebc15acc30884dc7d586debd421753d91dfa8c
NeoCat98 <00076017@uca.edu.sv>
2018-11-20 22:22:27 -0600

    Agregando foro

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var profileRouter = require('./routes/profile');
+var foroRouter = require('./routes/foro');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/profile',profileRouter);
+app.use('/foro',foroRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
