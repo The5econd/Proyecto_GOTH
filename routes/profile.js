153141c7c049c7fee867dd1432a70206b63ba45d
NeoCat98 <00076017@uca.edu.sv>
2018-11-20 19:43:54 -0600

    Pantalla de opciones

---
+++ routes/profile.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('profileGeneral',{ first_name:'Javier' , email:'00076017@uca.edu.sv'});
+});
+
+router.get('/General', function(req, res, next) {
+  res.render('profileGeneral',{ first_name:'Javier' , email:'00076017@uca.edu.sv'});
+});
+
+router.get('/Seguridad', function(req, res, next) {
+  res.render('profileSeguridad');
+});
+
+router.get('/Foros', function(req, res, next) {
+  res.render('profileForos');
+});
+
+router.get('/Playlist', function(req, res, next) {
+  res.render('profilePlaylist');
+});
+
+router.get('/Videos', function(req, res, next) {
+  res.render('profileVideo');
+});
+
+module.exports = router;
