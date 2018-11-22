5ce945660c794d9f1ed61614a349738fb0a2e96d
NeoCat98 <00076017@uca.edu.sv>
2018-11-21 20:28:51 -0600

    Agregando funcionamineto de botones

--- public/js/profile.js
+++ public/js/profile.js
@@ -7,3 +7,19 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.materialboxed');
     var instances = M.Materialbox.init(elems);
 });
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+});
+
+function Enable() {
+    var element = document.getElementsByClassName("enable");
+    for(var i=0;i<element.length;i++){
+        element[i].removeAttribute('disabled');
+     }
+};
