744e74c427a37953292737746d16f3d2bc1be888
NeoCat98 <00076017@uca.edu.sv>
2018-11-22 22:31:40 -0600

    Agregando el html y el css de la creacion de foros faltan hacer funcionar algunas cosas

--- public/js/profile.js
+++ public/js/profile.js
@@ -21,5 +21,50 @@ function Enable() {
     var element = document.getElementsByClassName("enable");
     for(var i=0;i<element.length;i++){
         element[i].removeAttribute('disabled');
-     }
+    }
 };
+
+function bold(){
+    var element = document.getElementsByClassName("bold");
+    var element1 = document.getElementsByClassName("tobold");
+    var pItem = document.createElement("p")
+    if(element[0].hasAttribute('style')){
+        element[0].removeClass('active');
+    }
+    else{
+        pItem.setAttribute('style',"font-weight: bold;");
+        element1.appendChild(pItem);
+    }
+};
+
+function italiac(){
+    var element = document.getElementsByClassName("italiac");
+    var element1 = document.getElementsByClassName("toItaliac");
+    var pItem = document.createElement("p")
+    if(element[0].hasAttribute('style')){
+        element[0].removeClass('active');
+
+    }
+    else{
+        pItem.setAttribute('style',"font-style: italic;");
+        element1.appendChild(pItem);
+    }
+};
+ function quitar(){
+    var element = document.getElementsByClassName("quitar");
+    var element1 = document.getElementsByClassName("toItaliac");
+    var pItem = document.createElement("p")
+    if(element[0].hasAttribute('style')){
+        element[0].removeClass('active');
+
+    }
+    else{
+        pItem.setAttribute('style',"font-style: italic;");
+        element1.appendChild(pItem);
+    }
+ }
+
+document.addEventListener('DOMContentLoaded',function(){
+    var elems = document.querySelectorAll('.tabs');
+    var instances = M.Tabs.init(elems);
+});
