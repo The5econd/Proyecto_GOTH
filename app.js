e2e086a716521922fba4a8a0ebe4dbbcf6a90e47
fernando <00017917@uca.edu.sv>
2018-11-26 10:08:46 -0600

    main page agregada con links funcionales

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var profileRouter = require('./routes/profile');
 var foroRouter = require('./routes/foro');
+var inicioRouter = require('./routes/inicio');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/profile',profileRouter);
 app.use('/foro',foroRouter);
+app.use('/inicio',inicioRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
