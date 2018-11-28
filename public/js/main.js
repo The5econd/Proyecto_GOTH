b672a290c6355392f548968b8806af47c3215a15
NeoCat98 <00076017@uca.edu.sv>
2018-11-27 18:40:49 -0600

    Agregando funcionamiento a la ventana playlist

--- public/js/main.js
+++ public/js/main.js
@@ -11,3 +11,28 @@ function guardar(){
                         <td>${data.Album}</td>`;
     tbody.appendChild(tr)
 };
+document.getElementById('myFile').onchange = function(){
+    var img = document.getElementsByClassName("imagenPlaylist");
+    var x = document.getElementById("myFile");
+    var txt = "";
+    if ('files' in x) {
+        if (x.files.length == 0) {
+            txt = "Select one or more files.";
+        } else {
+            for (var i = 0; i < x.files.length; i++) {
+                txt += "<br><strong>" + (i+1) + ". file</strong><br>";
+                var file = x.files[i];
+                var blob = new Blob([file], { type: 'application/javascript' });
+                var TmpPath = URL.createObjectURL(blob);
+                txt += "file: "+file+" URL: "+TmpPath;
+                if ('name' in file) {
+                    txt += "name: " + file.name + "<br>";
+                }
+                if ('size' in file) {
+                    txt += "size: " + file.size + " bytes <br>";
+                }
+            }
+        }
+        console.log(txt);
+    } 
+}
