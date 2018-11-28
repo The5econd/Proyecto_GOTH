710addc9a9de13b9fe817a609e39772a61787585
NeoCat98 <00076017@uca.edu.sv>
2018-11-27 19:55:14 -0600

    Creacion de modelos y controladores para la ventana playlist

---
+++ models/playlist.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose'); //Para manipular conexión y el manejo de la base de datos
+const cancionesModel = require("../models/canciones");
+
+const { Schema } = mongoose; //Objeto Schema para realizar diferentes operaciones
+const playlistSchema = new Schema({
+    //atributos con sus validaciones
+    titulo:String,
+    imagen: {type:"image/png"},
+    canciones: [cancionesModel]
+});
+
+
+let playlist = mongoose.model('playlist', playlistSchema);
+
+module.exports = playlist;
