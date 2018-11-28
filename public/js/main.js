773f10e0b2a4641d8326618a1f9f0c4cd7ba14bb
The5econd <00017917@uca.edu.sv>
2018-11-28 16:49:11 -0600

    prueba para estar seguro

--- public/js/main.js
+++ public/js/main.js
@@ -12,49 +12,28 @@ function guardar() {
                         <td>${data.Album}</td>`;
     tbody.appendChild(tr)
 };
-/*
-function mostrar() {
-    let idd = 'modal';
-    let cont = 1;
-    let hash = '#';
-    console.log('Aaaaaaaaaaaaaaahhhhh');
-    let data = {
-        tit: JSON.parse(session.publicaciones).titulo,
-        cuer: JSON.parse(session.publicaciones).texto
-    };
-    let tbody = document.getElementById("sec1");
-    let div = document.createElement("div");
-    div.innerHTML = `<div class="sectionn">
-                        <div class="">
-                            <div class="card-panel #80cbc4 teal lighten-3">
-                                <a class="linkss waves-effect waves-light btn modal-trigger" href="${hash+idd+cont}">
-                                    ${data.tit}</a>
 
-                                <div id=${idd+cont} class="modal">
-                                    <div class="modal-content inner_div">
-                                        <h4>${data.tit}</h4>
-                                        <p>${data.cuer}</p>
-                                    </div>           
-                                </div>
-                            </div>
-                        </div>
-                    </div>`;
-    tbody.appendChild(div);
-    cont = cont +1;
-};*/
+window.onload = () => {
+    app.init();
+}
+let app = {
+    init: function () {
+        this.mostrar();
+    },
 
-function mostrar() {
+    mostrar: function() {
     fetch('/api/post', {
         method: "GET"
     }).then(res => res.json())
         .then(response => {
             //var respo = [];
             // respo.add(response[i])
+            let cont = 1;
             console.log(response.length);
             for (let i = 0; i < 4; i++) {
                 //respo.add(respones.posts[i]);
                 let idd = 'modal';
-                let cont = 1;
+                
                 let hash = '#';
                 console.log(response.posts);
                 let data = {
@@ -64,24 +43,24 @@ function mostrar() {
                 let tbody = document.getElementById("sec1");
                 let div = document.createElement("div");
                 div.innerHTML = `<div class="sectionn">
-                        <div class="">
-                            <div class="card-panel #80cbc4 teal lighten-3">
-                                <a class="linkss waves-effect waves-light btn modal-trigger" href="${hash + idd + cont}">
-                                    ${data.tit}</a>
+                                    <div class="">
+                                        <div class="card-panel #80cbc4 teal lighten-3">
+                                            <a class="linkss waves-effect waves-light btn modal-trigger" href="${hash + idd + cont}">
+                                                ${data.tit}</a>
 
-                                <div id=${idd + cont} class="modal">
-                                    <div class="modal-content inner_div">
-                                        <h4>${data.tit}</h4>
-                                        <p>${data.cuer}</p>
-                                    </div>           
-                                </div>
-                            </div>
-                        </div>
-                    </div>`;
+                                            <div id=${idd + cont} class="modal">
+                                                <div class="modal-content inner_div">
+                                                    <h4>${data.tit}</h4>
+                                                    <p>${data.cuer}</p>
+                                                </div>           
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>`;
+                                cont = cont + 1;
                 tbody.appendChild(div);
-                cont = cont + 1;
             }
         })
 }
-
+}
 
