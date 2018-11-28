95bc958765797a8781a833138bb0b20ed58784fb
The5econd <00017917@uca.edu.sv>
2018-11-28 10:24:39 -0600

    ya muestra usuario y correo, foros-mongo en proceso

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-function guardar(){
+function guardar() {
     let data = {
         nombre: document.getElementById("nombre").value,
         Artista: document.getElementById("Artista").value,
@@ -12,3 +12,28 @@ function guardar(){
                         <td>${data.Album}</td>`;
     tbody.appendChild(tr)
 };
+
+function mostrar() {
+    let data = {
+        tit: JSON.parse(session.publicaciones).titulo,
+        cuer: JSON.parse(session.publicaciones).texto
+    };
+    let tbody = document.getElementById("sec1");
+    let sec = document.createElement("section");
+    sec.innerHTML = `<div class="sectionn">
+                        <div class="">
+                            <div class="card-panel #80cbc4 teal lighten-3">
+                                <a class="linkss waves-effect waves-light btn modal-trigger" href="#modal1">
+                                    ${data.tit}</a>
+
+                                <div id="modal1" class="modal">
+                                    <div class="modal-content inner_div">
+                                        <h4>${data.tit}</h4>
+                                        <p>${data.cuer}</p>
+                                    </div>           
+                                </div>
+                            </div>
+                        </div>
+                    </div>`;
+    tbody.appendChild(div);
+}
