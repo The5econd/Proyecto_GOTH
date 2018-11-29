79e288b7203cc4022e9cb9020bf79a942b2e942c
NeoCat98 <00076017@uca.edu.sv>
2018-11-29 13:06:33 -0600

    Creador de playlist funcionando

--- public/js/main.js
+++ public/js/main.js
@@ -9,9 +9,12 @@ function guardar() {
     let tr = document.createElement("tr");
     tr.className = "cancion";
     tr.id="tr";
-    tr.innerHTML = `<td>${data.nombre}</td>
-                        <td>${data.Artista}</td>
-                        <td>${data.Album}</td>`;
+    tr.innerHTML = `<td>
+                        <input id="nombre" name="nombre" value="${data.nombre}" type="text"></td>
+                    <td> 
+                    <input id="artista" name="artista" value="${data.Artista}" type="text"></td>
+                    <td>
+                    <input id="album" name="album" value="${data.Album}" type="text"></td>`;
     tbody.appendChild(tr)
 };
 
@@ -31,13 +34,13 @@ let app = {
                 //var respo = [];
                 // respo.add(response[i])
                 let cont = 1;
-                //console.log(response.length);
+                console.log(response.length);
                 for (let i = 0; i < 5; i++) {
                     //respo.add(respones.posts[i]);
                     let idd = 'modal';
 
                     let hash = '#';
-                    //console.log(response.posts);
+                    console.log(response.posts);
                     let data = {
                         tit: (response.posts[i]).titulo,
                         cuer: (response.posts[i]).texto,
@@ -82,9 +85,9 @@ function mostrar0() {
                                     <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
                                     <li class="active"><a href="#!">1</a></li>
                                     <li class="waves-effect"><a href="#!" onClick="mostrar1()">2</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar2()">3</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar3()">4</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar4()">5</a></li>
+                                    <li class="waves-effect"><a href="#!">3</a></li>
+                                    <li class="waves-effect"><a href="#!">4</a></li>
+                                    <li class="waves-effect"><a href="#!">5</a></li>
                                     <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
                                 </ul>`;
             let cont = 1;
@@ -138,14 +141,14 @@ function mostrar1() {
                                     <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
                                     <li class="waves-effect"><a href="#!" onClick='mostrar0()'>1</a></li>
                                     <li class="active"><a href="#!">2</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar2()">3</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar3()">4</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar4()">5</a></li>
+                                    <li class="waves-effect"><a href="#!">3</a></li>
+                                    <li class="waves-effect"><a href="#!">4</a></li>
+                                    <li class="waves-effect"><a href="#!">5</a></li>
                                     <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
                                 </ul>`;
             let cont = 1;
             console.log(response.length);
-            for (let i = 5; i < 10; i++) {
+            for (let i = 5; i < 9; i++) {
                 //respo.add(respones.posts[i]);
                 let idd = 'modal';
 
@@ -179,226 +182,3 @@ function mostrar1() {
         })
 }
 
-function mostrar2() {
-    fetch('/api/post', {
-        method: "GET"
-    }).then(res => res.json())
-        .then(response => {
-            //var respo = [];
-            // respo.add(response[i])
-            let parent = document.getElementById("sec1");
-            while (parent.firstChild) {
-                parent.removeChild(parent.firstChild);
-            };
-            parent.innerHTML = `<ul class="pagination" style="position: absolute; left: 25%; bottom: 0;">
-                                    <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar0()">1</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar1()">2</a></li>
-                                    <li class="active"><a href="#!">3</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar3()">4</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar4()">5</a></li>
-                                    <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
-                                </ul>`;
-            let cont = 1;
-            console.log(response.length);
-            for (let i = 10; i < 15; i++) {
-                //respo.add(respones.posts[i]);
-                let idd = 'modal';
-
-                let hash = '#';
-                console.log(response.posts);
-                let data = {
-                    tit: (response.posts[i]).titulo,
-                    cuer: (response.posts[i]).texto,
-                    aut: (response.posts[i]).autor
-                };
-                let tbody = document.getElementById("sec1");
-                let div = document.createElement("div");
-                div.innerHTML = `<div class="sectionn">
-                                    <div class="">
-                                        <div class="card-panel #80cbc4 teal lighten-3">
-                                            <a class="linkss waves-effect waves-light btn modal-trigger" href="${hash + idd + cont}">
-                                                ${data.tit}</a>
-                                            <a class="linkss waves effect waves-light">${data.aut}</a>
-                                            <div id=${idd + cont} class="modal">
-                                                <div class="modal-content inner_div">
-                                                    <h4>${data.tit}</h4>
-                                                    <p>${data.cuer}</p>
-                                                </div>           
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>`;
-                cont = cont + 1;
-                tbody.appendChild(div);
-            }
-        })
-}
-
-function mostrar3() {
-    fetch('/api/post', {
-        method: "GET"
-    }).then(res => res.json())
-        .then(response => {
-            //var respo = [];
-            // respo.add(response[i])
-            let parent = document.getElementById("sec1");
-            while (parent.firstChild) {
-                parent.removeChild(parent.firstChild);
-            };
-            parent.innerHTML = `<ul class="pagination" style="position: absolute; left: 25%; bottom: 0;">
-                                    <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar0()">1</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar1()">2</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar2()">3</a></li>
-                                    <li class="active"><a href="#!">4</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar4()">5</a></li>
-                                    <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
-                                </ul>`;
-            let cont = 1;
-            console.log(response.length);
-            for (let i = 15; i < 20; i++) {
-                //respo.add(respones.posts[i]);
-                let idd = 'modal';
-
-                let hash = '#';
-                console.log(response.posts);
-                let data = {
-                    tit: (response.posts[i]).titulo,
-                    cuer: (response.posts[i]).texto,
-                    aut: (response.posts[i]).autor
-                };
-                let tbody = document.getElementById("sec1");
-                let div = document.createElement("div");
-                div.innerHTML = `<div class="sectionn">
-                                    <div class="">
-                                        <div class="card-panel #80cbc4 teal lighten-3">
-                                            <a class="linkss waves-effect waves-light btn modal-trigger" href="${hash + idd + cont}">
-                                                ${data.tit}</a>
-                                            <a class="linkss waves effect waves-light">${data.aut}</a>
-                                            <div id=${idd + cont} class="modal">
-                                                <div class="modal-content inner_div">
-                                                    <h4>${data.tit}</h4>
-                                                    <p>${data.cuer}</p>
-                                                </div>           
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>`;
-                cont = cont + 1;
-                tbody.appendChild(div);
-            }
-        })
-}
-
-function mostrar3() {
-    fetch('/api/post', {
-        method: "GET"
-    }).then(res => res.json())
-        .then(response => {
-            //var respo = [];
-            // respo.add(response[i])
-            let parent = document.getElementById("sec1");
-            while (parent.firstChild) {
-                parent.removeChild(parent.firstChild);
-            };
-            parent.innerHTML = `<ul class="pagination" style="position: absolute; left: 25%; bottom: 0;">
-                                    <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar0()">1</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar1()">2</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar2()">3</a></li>
-                                    <li class="active"><a href="#!">4</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar4()">5</a></li>
-                                    <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
-                                </ul>`;
-            let cont = 1;
-            console.log(response.length);
-            for (let i = 20; i < 25; i++) {
-                //respo.add(respones.posts[i]);
-                let idd = 'modal';
-
-                let hash = '#';
-                console.log(response.posts);
-                let data = {
-                    tit: (response.posts[i]).titulo,
-                    cuer: (response.posts[i]).texto,
-                    aut: (response.posts[i]).autor
-                };
-                let tbody = document.getElementById("sec1");
-                let div = document.createElement("div");
-                div.innerHTML = `<div class="sectionn">
-                                    <div class="">
-                                        <div class="card-panel #80cbc4 teal lighten-3">
-                                            <a class="linkss waves-effect waves-light btn modal-trigger" href="${hash + idd + cont}">
-                                                ${data.tit}</a>
-                                            <a class="linkss waves effect waves-light">${data.aut}</a>
-                                            <div id=${idd + cont} class="modal">
-                                                <div class="modal-content inner_div">
-                                                    <h4>${data.tit}</h4>
-                                                    <p>${data.cuer}</p>
-                                                </div>           
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>`;
-                cont = cont + 1;
-                tbody.appendChild(div);
-            }
-        })
-}
-
-function mostrar4() {
-    fetch('/api/post', {
-        method: "GET"
-    }).then(res => res.json())
-        .then(response => {
-            //var respo = [];
-            // respo.add(response[i])
-            let parent = document.getElementById("sec1");
-            while (parent.firstChild) {
-                parent.removeChild(parent.firstChild);
-            };
-            parent.innerHTML = `<ul class="pagination" style="position: absolute; left: 25%; bottom: 0;">
-                                    <li class="disabled"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar0()">1</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar1()">2</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar2()">3</a></li>
-                                    <li class="waves-effect"><a href="#!" onClick="mostrar3()">4</a></li>
-                                    <li class="active"><a href="#!">5</a></li>
-                                    <li class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
-                                </ul>`;
-            let cont = 1;
-            console.log(response.length);
-            for (let i = 25; i < 30; i++) {
-                //respo.add(respones.posts[i]);
-                let idd = 'modal';
-
-                let hash = '#';
-                console.log(response.posts);
-                let data = {
-                    tit: (response.posts[i]).titulo,
-                    cuer: (response.posts[i]).texto,
-                    aut: (response.posts[i]).autor
-                };
-                let tbody = document.getElementById("sec1");
-                let div = document.createElement("div");
-                div.innerHTML = `<div class="sectionn">
-                                    <div class="">
-                                        <div class="card-panel #80cbc4 teal lighten-3">
-                                            <a class="linkss waves-effect waves-light btn modal-trigger" href="${hash + idd + cont}">
-                                                ${data.tit}</a>
-                                            <a class="linkss waves effect waves-light">${data.aut}</a>
-                                            <div id=${idd + cont} class="modal">
-                                                <div class="modal-content inner_div">
-                                                    <h4>${data.tit}</h4>
-                                                    <p>${data.cuer}</p>
-                                                </div>           
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>`;
-                cont = cont + 1;
-                tbody.appendChild(div);
-            }
-        })
-}
